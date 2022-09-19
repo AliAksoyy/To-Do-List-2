d4a2b2f46951564a7b695f16e66cae9b983911aa
Ali <alifeyza4017@gmail.com>
2022-09-19 16:46:00 +0300

    added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", () => {
    const form = document.querySelector("#new-task-form")
-   console.log(form)
+
    const input = document.querySelector("#new-task-input")
    const list_el = document.querySelector("#tasks")
 
-   form.addEventListener("submit", (e) => {
-     console.log(e.target);
-   });
+  form.addEventListener("submit", (e) => {
+   e.preventDefault()
+   
+  })
 
 
 })
